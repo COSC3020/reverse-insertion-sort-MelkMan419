31783cca80ba13259f1699b084c9dff4c9bf8076
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-01-24 16:23:42 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function insertionSortReverse(arr) {
-  for (var i = arr.length - 2; i >= 0; i--) {
+  for(var i = arr.length - 2; i >= 0; i--) {
     var val = arr[i];
     var j;
-    for (j = i; j < arr.length - 1 && arr[j + 1] < val; j++) {
-      arr[j + 1] = arr[j];
+    for(j = i; j < arr.length - 1 && arr[j+1] < val; j++) {
+      arr[j] = arr[j+1];
     }
     arr[j] = val;
   }
